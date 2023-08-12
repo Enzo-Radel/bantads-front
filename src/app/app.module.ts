5796249db53e7c554c1fe37cfa0ade2c4ee40552
Enzo-Radel <enzo@radel.com.br>
2023-08-12 20:01:14 -0300

    refactor: change header location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './views/layout/header/header.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { ClientModule } from './client/client.module';
 import { ModalComponent } from './components/modal/modal.component';
 
