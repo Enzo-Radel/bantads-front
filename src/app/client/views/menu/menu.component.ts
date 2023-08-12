837039bbc913ca07d24577cfb70b0c9ce3d8061e
Enzo-Radel <enzo@radel.com.br>
2023-08-12 13:30:12 -0300

    feat: create view client menu

--- src/app/client/views/menu/menu.component.ts
+++ src/app/client/views/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import Client from '../../entities/Client';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-
+    client = Client.findClient(1);
 }
