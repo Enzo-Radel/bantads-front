6f3aed757e715335cfdac2ad2facad29e173073e
Enzo-Radel <enzo@radel.com.br>
2023-08-12 18:16:31 -0300

    feat: create modal to deposit and transfer

--- src/app/client/views/menu/menu.component.ts
+++ src/app/client/views/menu/menu.component.ts
@@ -20,7 +20,36 @@ export class MenuComponent {
 
         formData.append("withdraw_value", valueInput.value);
 
-        console.log(formData.get("withdraw_value"));
+        console.log("valor: " + formData.get("withdraw_value"));
+
+        // TODO Recuperar "action" da variável form e fazer requisição async
+    }
+
+    deposit() {
+        let form = document.getElementById("deposit_form_id")!;
+        let valueInput = document.getElementById("deposit_value_input")! as HTMLInputElement;
+
+        let formData = new FormData();
+
+        formData.append("deposit_value", valueInput.value);
+
+        console.log("valor: " + formData.get("deposit_value"));
+
+        // TODO Recuperar "action" da variável form e fazer requisição async
+    }
+
+    transference() {
+        let form = document.getElementById("transference_form_id")!;
+        let valueInput = document.getElementById("transference_value_input")! as HTMLInputElement;
+        let accountInput = document.getElementById("transference_account_input")! as HTMLInputElement;
+
+        let formData = new FormData();
+
+        formData.append("transference_value", valueInput.value);
+        formData.append("transference_account", accountInput.value);
+
+        console.log("conta: " + formData.get("transference_account"));
+        console.log("valor: " + formData.get("transference_value"));
 
         // TODO Recuperar "action" da variável form e fazer requisição async
     }
