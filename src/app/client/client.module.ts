2e6c765df180d3335d8ae3531e98514fac45830e
Enzo-Radel <enzo@radel.com.br>
2023-08-12 21:32:06 -0300

    refactor: change client form component location

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './views/register/register.component';
 import { ProfileComponent } from './views/profile/profile.component';
-import { FormComponent } from './views/form/form.component';
+import { FormComponent } from './components/form/form.component';
 import { MenuComponent } from './views/menu/menu.component';
 import { ClientRoutingModule } from './client-routing.module';
 import { LoginComponent } from './views/login/login.component';
